41b988a8454850353fb02f848d4f2d9a36acefc0
Greg Rickaby <gregrickaby@gmail.com>
2012-03-07 14:59:19 -0600

    v1.0.0

--- lib/scripts/timthumb-config.php
+++ lib/scripts/timthumb-config.php
@@ -11,8 +11,8 @@
  * More Info: http://www.binarymoon.co.uk/projects/timthumb/
  */
 if(! defined( 'FILE_CACHE_DIRECTORY' ) ) define ( 'FILE_CACHE_DIRECTORY', '../../custom/cache' ); // Sets the cache directory
-if(! defined( 'DEFAULT_Q' ) ) define ( 'DEFAULT_Q', 70 ); // Default image quality (up to 100). The higher, the better.
+if(! defined( 'DEFAULT_Q' ) ) define ( 'DEFAULT_Q', 85 ); // Default image quality (up to 100). The higher, the better.
 if(! defined( 'DEFAULT_ZC' ) ) define ( 'DEFAULT_ZC', 1 ); // Default zoom/crop setting.
 if(! defined( 'DEFAULT_F' ) ) define ( 'DEFAULT_F', '' ); // Default image filters.
 if(! defined( 'DEFAULT_S' ) ) define ( 'DEFAULT_S', 0 ); // Default sharpen value.
-if(! defined( 'DEFAULT_CC' ) ) define ( 'DEFAULT_CC', 'ffffff' ); // Default canvas color.
+if(! defined( 'DEFAULT_CC' ) ) define ( 'DEFAULT_CC', '000000' ); // Default canvas color.
